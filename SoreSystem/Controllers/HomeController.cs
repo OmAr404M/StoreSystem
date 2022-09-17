fba4787a8eed7622bcdf3c435ee1b2d1fd4d46d8
Omar Abdelhameed <Omar_201900515@fci.helwan.edu.eg>
2022-09-17 07:16:46 -0700

    initial commit

---
+++ SoreSystem/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoreSystem.Models;
+using System.Collections.Generic;
+
+namespace SoreSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+       public List<Items> CreateListItems()
+        {
+            List<Items> LstItems = new List<Items>();
+            Items oItems = new Items();
+            oItems.ItemId = 1;
+            oItems.ItemName = "Hp zbook g3";
+            oItems.SalesPrice = 7500;
+            oItems.PurchasePice = 6500;
+            LstItems.Add(oItems);
+
+            oItems = new Items();
+            oItems.ItemId = 2;
+            oItems.ItemName = "Hp zbook g2";
+            oItems.SalesPrice = 12500;
+            oItems.PurchasePice = 11500;
+            LstItems.Add(oItems);
+
+            oItems = new Items();
+            oItems.ItemId = 3;
+            oItems.ItemName = " dell 7510";
+            oItems.SalesPrice = 10500;S
+            oItems.PurchasePice = 9000;
+            LstItems.Add(oItems);
+
+            oItems = new Items();
+            oItems.ItemId = 4;
+            oItems.ItemName = "dell vestro ";
+            oItems.SalesPrice = 8000;
+            oItems.PurchasePice = 6500;
+            LstItems.Add(oItems);
+            return LstItems;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.pageTitle = "Buy a new Item from here";
+            List<Items> LstItems = new List<Items>();
+            Items oItems = new Items();
+            oItems.ItemId = 1;
+            oItems.ItemName = "Hp zbook g3";
+            oItems.SalesPrice = 7500;
+            oItems.PurchasePice = 6500;
+            LstItems.Add(oItems);   
+            return View(CreateListItems());
+        }
+
+
+    }
+}
---
+++ SoreSystem/Models/Items.cs
@@ -0,0 +1,11 @@
+﻿namespace SoreSystem.Models
+{
+    public class Items
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int CategoryId { get; set; }
+        public decimal SalesPrice { get; set; }
+        public decimal PurchasePice { get; set; }
+    }
+}
